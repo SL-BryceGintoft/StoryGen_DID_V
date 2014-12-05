41bdcb4d2f2b5481d8d694ad51debc100bb581d7
SL-BGintoft <bgintoft@shoplocal.com>
2014-12-04 19:27:36 -0600

    reading and writing functionality

--- Content/Scripts/sl.sc.application.js
+++ Content/Scripts/sl.sc.application.js
@@ -10,23 +10,60 @@ sl.sc.application = new function () {
 
     //# Public Vars
     {
-        this.somevar = "blah";
-        
+        this.featuresFile = "http://localhost/Content/Data/features.json";
+        this.storiesFile = "http://localhost/Content/Data/stories.json";
+        this.localStoreFeatures = "Features";
+        this.localStoreStories = "Stories";
     }
 
     //# Public Methods
 
     {
-        this.init = function () {
-            alert("alive");
+
+        this.setLocalStorageItem = function (key, value) {
+            localStorage.setItem(key, JSON.stringify(value));
         };
 
+        this.getLocalStorageItem = function (key) {
+            var retrievedObject = localStorage.getItem(key);
+            if (retrievedObject)
+                return JSON.parse(retrievedObject);
+            return null;
+        };
+
+        this.getJsonData = function (file,cb) {
+            console.log(file);
+            $.ajax( file, {
+                format: "json"
+            })
+            .done(function( data ) {
+                cb(data);
+            }).error(function(jqXhr, textStatus, error) {
+                alert("ERROR: " + textStatus + ", " + error);
+            });
+        };
+        
+        this.renderFeatures = function (data) {
+            console.log(data);
+            //console.log(sl.sc.application.localStoreFeatures);
+            //sl.sc.application.setLocalStorageItem(sl.sc.application.localStoreFeatures,data);
+            //var test = sl.sc.application.getLocalStorageItem(sl.sc.application.localStoreFeatures);
+            //console.log(test);
+        };
+        this.renderStories = function (data) {
+            console.log(data);
+            //console.log(JSON.stringify(data));
+        };
+
+        this.init = function () {
+            sl.sc.application.getJsonData(sl.sc.application.featuresFile, sl.sc.application.renderFeatures);
+            //sl.sc.application.getJsonData(sl.sc.application.storiesFile, sl.sc.application.renderStories);
+        };
 
         this.ready = function () {
             sl.sc.application.init();
 
         };
-
     }
 
     $(document).ready(this.ready);
